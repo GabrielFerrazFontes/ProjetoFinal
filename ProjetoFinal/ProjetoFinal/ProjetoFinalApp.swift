bf2b2b8c176747d74da98c2f58cc0b800c3c0198
Gabriel Ferraz Fontes <brielff@gmail.com>
2025-12-17 15:33:25 -0300

    Primeiro Commit
    Adicionando o Projeto ao Github

---
+++ ProjetoFinal/ProjetoFinal/ProjetoFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoFinalApp.swift
+//  ProjetoFinal
+//
+//  Created by Gabriel fontes on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
