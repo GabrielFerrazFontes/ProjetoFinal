1dd5ecdfee77dd3b6bc97918eef32bc2b75d0814
Gabriel Ferraz Fontes <brielff@gmail.com>
2025-12-18 11:08:35 -0300

    Alterando o texto

--- ProjetoFinalXcode/ProjetoFinal/ContentView.swift
+++ ProjetoFinalXcode/ProjetoFinal/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Nova Alteração!")
+            Text("Alterando de novo")
         }
         .padding()
     }
