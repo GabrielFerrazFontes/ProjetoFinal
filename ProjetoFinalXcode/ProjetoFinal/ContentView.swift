afdb03c7c545860e5716b94726b78b0bf3be74b6
Gabriel Ferraz Fontes <brielff@gmail.com>
2025-12-18 11:06:50 -0300

    Update ContentView

--- ProjetoFinalXcode/ProjetoFinal/ContentView.swift
+++ ProjetoFinalXcode/ProjetoFinal/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Nova Alteração!")
+            Text("Outra Pessoa Alterou o Texto")
         }
         .padding()
     }
